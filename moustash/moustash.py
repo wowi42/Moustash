d60700823d5af65d8bbad96b4859edcd02994e11
Loic Tosser <tosser.loic@gmail.com>
2014-05-26 10:27:13 +0200

    Fix moustash

--- moustash/moustash.py
+++ moustash/moustash.py
@@ -11,13 +11,14 @@ import platform
 import time
 
 class Moustash:
-    def __init__(self):
+    def __init__(self, message, tags, type_stash, program, source, notification):
         self.moustash = {}
         self.moustash["tags"] = []
+        self.moustash["tags"].append(tags)
         self.moustash["@timestamp"] = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
         self.moustash["host"] = platform.node()
-        self.moustash["message"] = ""
-        self.moustash["type"] = ""
-        self.moustash["program"] = ""
-        self.moustash["source"] = ""
-        self.moustash["notification"] = True
+        self.moustash["message"] = message
+        self.moustash["type"] = type_stash
+        self.moustash["program"] = program
+        self.moustash["source"] = source
+        self.moustash["notification"] = notification
