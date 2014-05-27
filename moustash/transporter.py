2e649884abbaca694e00215568830136860cf7ae
L. Tosser wowi <loic@findspire.com>
2014-05-27 12:01:38 +0200

    Fix rabbitMQ options

--- moustash/transporter.py
+++ moustash/transporter.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #config.py for Config File in /home/loic/GitHub/Moustash/moustash
 #
-#Made by Loic Tosser, Findspire.com
+#Made by Loic Tosser, Wowi.io
 #Login   <loic@wowi.io>
 #
 #
@@ -18,41 +18,41 @@ class Franck:
         self.moustash_config = {}
         self.moustash_config["Moustash"] = config_section_map(self.config, "Moustash")
         self.moustash_config["Cuir"] = config_section_map(self.config, "Cuir")
+        self.broker = {}
         if self.moustash_config["Moustash"]["transport"] == "redis":
-            self.broker = self.connect_to_redis()
-            redis_namespace = "moustash"
-            if "redis_namespace" in self.moustash_config["Moustash"]:
-                redis_namespace = self.moustash_config["Moustash"]["redis_namespace"]
-            self.broker_namespace = redis_namespace
+            redis_parameters = {"redis_host" : "localhost", "redis_port" : "6379", "redis_db" : "0", "redis_namespace": "logstash:moustash"}
+            self.fill_broker_options(redis_parameters)
+            self.broker_connection = self.connect_to_redis()
         else if self.moustash_config["Moustash"]["transport"] == "rabbitmq":
-            self.broker = {}
-            rabbitmq_parameters = {"rabbitmq_host" : "localhost", "rabbitmq_port": "5672", "rabbitmq_ssl" : "0", "rabbitmq_ssl_key" = None, 
-                                   "rabbitmq_ssl_cert" = None, "rabbitmq_ssl_cacert" = None, "rabbitmq_vhost" = "/", "rabbitmq_username" = "guest",
-                                   "rabbitmq_password" = "guest", "rabbitmq_queue" = "logstash-queue", "rabbitmq_queue_durable" = "0",
-                                   "rabbitmq_exchange_type" = "direct", "rabbitmq_exchange_durable" = "0", "rabbitmq_key" = "logstash-key",
-                                   "rabbitmq_exchange" = "logstash-exchange"}
-            for rabbitmq_options, rabbitmq_options_value in rabbitmq_parameters.iteritems():
-                if rabbitmq_options in self.moustash_config["Moustash"]:
-                    self.broker[rabbitmq_options] = self.moustash_config["Moustash"][rabbitmq_options]
-                else:
-                    self.broker[rabbitmq_options] = rabbitmq_options_value
+            rabbitmq_parameters = {"rabbitmq_host" : "localhost", "rabbitmq_port": "5672",
+                                   "rabbitmq_ssl" : "0", "rabbitmq_ssl_key" = None, 
+                                   "rabbitmq_ssl_cert" = None, "rabbitmq_ssl_cacert" = None,
+                                   "rabbitmq_vhost" = "/", "rabbitmq_username" = "guest",
+                                   "rabbitmq_password" = "guest",
+                                   "rabbitmq_queue" = "logstash-queue", "rabbitmq_queue_durable" = "0",
+                                   "rabbitmq_exchange_type" = "direct", "rabbitmq_exchange_durable" = "0",
+                                   "rabbitmq_key" = "logstash-key", "rabbitmq_exchange" = "logstash-exchange"}
+            self.fill_broker_options(rabbitmq_parameters)
+            self.broker_connection = self.connect_to_rabbitmq()
         else:
             print("Not yet implemented : %s !" % moustash_config["Moustash"]["transport"])
-    
+            
+
+    def fill_broker_options(self, options):
+        for option_name, option_value in options.iteritems():
+            if option_name in self.moustash_config["Moustash"]:
+                self.broker[option_name] = self.moustash_config["Moustash"][option_name]
+            else:
+                self.broker[option_name] = option_value
+
     def connect_to_redis(self):
-        redis_ip = "127.0.0.1"
-        redis_port = "6379"
-        redis_db = "0"
-        if "redis_ip" in self.moustash_config["Moustash"]:
-            redis_ip = self.moustash_config["Moustash"]["redis_ip"]
-        if "redis_port" in self.moustash_config["Moustash"]:
-            redis_port = self.moustash_config["Moustash"]["redis_port"]
-        if "redis_db" in self.moustash_config["Moustash"]:
-            redis_db = self.moustash_config["Moustash"]["redis_db"]
-        r = redis.StrictRedis(host=redis_ip, port=int(redis_port), db=int(redis_db))
+        r = redis.StrictRedis(host=self.broker["redis_ip"], port=int(self.broker["redis_port"]), db=int(self.broker["redis_db"]))
+        return r
+
+    def connect_to_rabbitmq(self):
+        r = redis.StrictRedis(host=self.broker["redis_ip"], port=int(self.broker["redis_port"]), db=int(self.broker["redis_db"]))
         return r
 
     def push_moustash(self, json_message):
         if self.moustash_config["Moustash"]["transport"] == "redis":
-            self.broker.rpush(self.broker_namespace, json_message)
-
+            self.broker.rpush(self.broker["redis_namespace"], json_message)
